8e07c77e6630359530793b1cc49ebffb9f54eafb
Brandon Rubio <brubio3724@gmail.com>
2022-06-02 16:33:55 -0400

    Added components for rankings page

---
+++ src/pages/Rankings.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { useFetch } from '../utils/useFetch';
+
+const { state } = await useFetch('https://hltv-rest-api.herokuapp.com/teams')
+</script>
+<template>
+<div v-for="player in state.response">{{player.id}}</div>
+</template>
+<style>
+</style>
