589da94e9880bd8c69ba3bea1246a03498010ebe
Brandon Rubio <brubio3724@gmail.com>
2022-06-11 14:27:44 -0400

    Added some data for testing

---
+++ src/utils/testState.ts
@@ -0,0 +1,115 @@
+import { Ref, ref } from "vue";
+import { IState } from "../interfaces/types";
+
+export function testState(): Ref<IState> {
+    return (ref<IState>({
+        response: [
+            {
+                "id": 6667,
+                "ranking": 1,
+                "name": "FaZe",
+                "logo": "https://img-cdn.hltv.org/teamlogo/SMhzsxzbkIrgqCOOKGRXlW.svg?ixlib=java-2.1.0&s=e6a9ce0345c7d703e5eaac14307f69aa",
+                "players": [
+                    {
+                        "fullname": "Finn 'karrigan' Andersen",
+                        "image": "",
+                        "nickname": "karrigan",
+                        "country": {
+                        "name": "Denmark",
+                        "flag": "/img/static/flags/30x20/DK.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Håvard 'rain' Nygaard",
+                        "image": "",
+                        "nickname": "rain",
+                        "country": {
+                        "name": "Norway",
+                        "flag": "/img/static/flags/30x20/NO.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Russel 'Twistzz' Van Dulken",
+                        "image": "",
+                        "nickname": "Twistzz",
+                        "country": {
+                        "name": "Canada",
+                        "flag": "/img/static/flags/30x20/CA.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Robin 'ropz' Kool",
+                        "image": "",
+                        "nickname": "ropz",
+                        "country": {
+                        "name": "Estonia",
+                        "flag": "/img/static/flags/30x20/EE.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Helvijs 'broky' Saukants",
+                        "image": "",
+                        "nickname": "broky",
+                        "country": {
+                        "name": "Latvia",
+                        "flag": "/img/static/flags/30x20/LV.gif"
+                        }
+                    }
+                ]
+            },
+            {
+                "id": 6667,
+                "ranking": 1,
+                "name": "FaZe",
+                "logo": "https://img-cdn.hltv.org/teamlogo/SMhzsxzbkIrgqCOOKGRXlW.svg?ixlib=java-2.1.0&s=e6a9ce0345c7d703e5eaac14307f69aa",
+                "players": [
+                    {
+                        "fullname": "Finn 'karrigan' Andersen",
+                        "image": "",
+                        "nickname": "karrigan",
+                        "country": {
+                        "name": "Denmark",
+                        "flag": "/img/static/flags/30x20/DK.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Håvard 'rain' Nygaard",
+                        "image": "",
+                        "nickname": "rain",
+                        "country": {
+                        "name": "Norway",
+                        "flag": "/img/static/flags/30x20/NO.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Russel 'Twistzz' Van Dulken",
+                        "image": "",
+                        "nickname": "Twistzz",
+                        "country": {
+                        "name": "Canada",
+                        "flag": "/img/static/flags/30x20/CA.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Robin 'ropz' Kool",
+                        "image": "",
+                        "nickname": "ropz",
+                        "country": {
+                        "name": "Estonia",
+                        "flag": "/img/static/flags/30x20/EE.gif"
+                        }
+                    },
+                    {
+                        "fullname": "Helvijs 'broky' Saukants",
+                        "image": "",
+                        "nickname": "broky",
+                        "country": {
+                        "name": "Latvia",
+                        "flag": "/img/static/flags/30x20/LV.gif"
+                        }
+                    }
+                ]
+            }
+        ]
+    }))
+}
