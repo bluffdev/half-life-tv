e220e653a3e865a8d1f02fe8e7fc25572a783f09
Brandon Rubio <brubio3724@gmail.com>
2022-06-18 12:12:32 -0400

    Added player's flag

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -9,7 +9,10 @@ defineProps<{ player: IPlayer }>()
     <div class="player">
         <!-- <img src="../assets/karrigan.png" class="player-image"/> -->
         <img :src="player.image" class="player-image" />
-        <div class="player-nickname">{{player.nickname}}</div>
+        <div class="player-info">
+            <img :src="player.country.flag" class="player-flag" />
+            <div class="player-nickname">{{player.nickname}}</div>
+        </div>
     </div>
 </template>
 
@@ -19,12 +22,26 @@ defineProps<{ player: IPlayer }>()
     margin-top: 10px;
 }
 
+.player-info {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-bottom: 4.5px;
+}
+
 .player-image {
     width: 7.5em;
     height: auto;
 }
 
+.player-flag {
+    width: 1.25em;
+    height: auto;
+    margin: 0;
+    margin-right: 0.25em;
+}
+
 .player-nickname {
-    margin-bottom: 4.5px;
+    margin: 0;
 }
 </style>
